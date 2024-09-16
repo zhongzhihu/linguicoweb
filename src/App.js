3a4571d1e67f0ae1c7505c3531d7905dc586208a
Zhongzhi Hu <zhongzhihu@hotmail.com>
2024-09-16 15:01:48 +0200

    Linguico web init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./linguicoLogo.png"; // You can replace this with your app's logo
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Linguico logo" />
+        <h1>Welcome to Linguico</h1>
+        <p>Learn German easily with our iOS and Android app, Linguico!</p>
+        <p>Available for download on the App Store and Google Play.</p>
+        <div className="app-links">
+          <a
+            className="App-link"
+            href="https://apps.apple.com/us/app/id6578450704"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download on the App Store
+          </a>
+          <a
+            className="App-link"
+            href="https://play.google.com/store/apps/details?id=com.linguistic.Linguistic&gl=cn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Get it on Google Play
+          </a>
+        </div>
       </header>
     </div>
   );
