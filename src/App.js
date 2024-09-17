019ad299eaf86e617f1eeea00d21b41dd8a749a6
Zhongzhi Hu <zhongzhihu@hotmail.com>
2024-09-17 10:24:08 +0200

    add more screenshots and render screenshot 6 on user in CH

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import logo from "./linguicoLogo.png";
 import appStoreButton from "./download-app-store-black.svg";
 import googlePlayButton from "./download-play-store.png";
@@ -5,9 +7,30 @@ import swissMade from "./SwissMadeDark.png";
 import screenshot1 from "./screenshot1.png";
 import screenshot2 from "./screenshot2.png";
 import screenshot3 from "./screenshot3.png";
+import screenshot4 from "./screenshot4.png";
+import screenshot5 from "./screenshot5.png";
+import screenshot6 from "./screenshot6.png";
+
 import "./App.css";
 
 function App() {
+  const [isSwissUser, setIsSwissUser] = useState(false);
+
+  useEffect(() => {
+    // Fetch the user's geolocation data
+    axios
+      .get("https://ipapi.co/json/")
+      .then((response) => {
+        const country = response.data.country;
+        if (country === "CH") {
+          setIsSwissUser(true); // User is from Switzerland
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching geolocation data:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -47,6 +70,11 @@ function App() {
           <img src={screenshot1} alt="Screenshot 1" className="Screenshot" />
           <img src={screenshot2} alt="Screenshot 2" className="Screenshot" />
           <img src={screenshot3} alt="Screenshot 3" className="Screenshot" />
+          <img src={screenshot4} alt="Screenshot 4" className="Screenshot" />
+          <img src={screenshot5} alt="Screenshot 5" className="Screenshot" />
+          {isSwissUser && (
+            <img src={screenshot6} alt="Screenshot 6" className="Screenshot" />
+          )}
         </div>
       </header>
     </div>
