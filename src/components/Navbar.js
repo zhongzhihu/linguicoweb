1e372a37be5c1e01493243b0f6ef0860cdbbb74d
Zhongzhi Hu <zhongzhihu@hotmail.com>
2024-09-24 13:13:10 +0200

    change navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
-import React from "react";
-import linguicoLogo from "../linguicoLogo.png"; // Import your custom logo
+import React, { useState } from "react";
+import linguicoLogo from "../linguicoLogo.png";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="navbar-brand">
-          <img src={linguicoLogo} alt="Linguico Logo" className="navbar-logo" />
+        <div className="navbar-mobile-section">
+          <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="navbar-brand">
+            <img
+              src={linguicoLogo}
+              alt="Linguico Logo"
+              className="navbar-logo"
+            />
+          </div>
         </div>
-        <div className="navbar-links">
+        <div className={`navbar-links ${isOpen ? "active" : ""}`}>
           <a href="#home">Home</a>
           <a href="#blog">Blog</a>
           <a href="#projects">Projects</a>
